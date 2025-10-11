e6290451732d4076b0b9282b06bd8efd2a3b5378
sofiaparreira <sofiapparreira@gmail.com>
2025-10-11 14:37:02 -0300

    alteração na configuração do database

--- database/conn.js
+++ database/conn.js
@@ -4,7 +4,7 @@ async function main() {
     try {
         mongoose.set("strictQuery", true)
         await mongoose.connect(
-            "mongodb+srv://user_default:a123456@cluster0.ssv50h1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb+srv://user_default:a123456@cluster0.ssv50h1.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
         );
 
         console.log("Conectado ao banco!")
